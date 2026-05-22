386e2c2f0394ab477d6a14e3fba02e94b8e1dd80
Anton Aliaksandrau <antonalex2021@gmail.com>
2026-05-22 19:32:53 +0300

    last update

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -4,3 +4,24 @@
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чётных чисел от 1 до N.
 
+#include "logic.h"
+
+int recursion(int number) {
+	if (number == 2)
+	{
+		return 2;
+	}
+	
+	if (number % 2 == 0) {
+
+	}
+}
+
+int sum(int number) {
+	if (number < 0) 
+	{
+		return -1;
+	}
+
+	return recursion(number);
+}
