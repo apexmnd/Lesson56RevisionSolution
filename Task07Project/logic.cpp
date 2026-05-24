63a3df62de8300c55be9c60e04ba9f0fccaef7f4
Anton Aliaksandrau <antonalex2021@gmail.com>
2026-05-24 19:23:55 +0300

    task 07 done

--- Task07Project/logic.cpp
+++ Task07Project/logic.cpp
@@ -5,3 +5,22 @@
 // которая считает количество нечётных цифр
 // в заданном целом числе.
 
+#include "logic.h"
+
+int recursion(int number) {
+	if (number <= 9)
+	{
+		return number % 2 == 0 ? 0 : 1;
+	}
+
+	return recursion(number / 10) + (number % 2 == 0 ? 0 : 1);
+}
+
+int count_of_odd_digits(int number) {
+	if (number < 0)
+	{
+		number *= -1;
+	}
+
+	return recursion(number);
+}
